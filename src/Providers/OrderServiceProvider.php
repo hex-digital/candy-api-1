4b0f0a3ebdaa4b5294e97aca4d8b1ac84c767e11
Alec Ritson <hello@itsalec.co.uk>
2019-01-16 10:14:21 +0000

    Add additional bindings

--- src/Providers/OrderServiceProvider.php
+++ src/Providers/OrderServiceProvider.php
@@ -4,6 +4,8 @@ namespace GetCandy\Api\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use GetCandy\Api\Core\Orders\OrderCriteria;
+use GetCandy\Api\Core\Orders\Services\OrderService;
+use GetCandy\Api\Core\Orders\Interfaces\OrderServiceInterface;
 use GetCandy\Api\Core\Orders\Interfaces\OrderCriteriaInterface;
 
 class OrderServiceProvider extends ServiceProvider
@@ -12,6 +14,10 @@ class OrderServiceProvider extends ServiceProvider
     {
         $this->app->bind(OrderCriteriaInterface::class, function ($app) {
             return $app->make(OrderCriteria::class);
+        });
+
+        $this->app->bind(OrderServiceInterface::class, function ($app) {
+            return $app->make(OrderService::class);
         });
     }
 }

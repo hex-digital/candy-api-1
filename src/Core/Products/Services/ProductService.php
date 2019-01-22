fede1f0c0e5ef3910882f40cec423a519a7bbd89
Alec Ritson <hello@itsalec.co.uk>
2019-01-22 17:29:33 +0000

    Added some eager loading

--- src/Core/Products/Services/ProductService.php
+++ src/Core/Products/Services/ProductService.php
@@ -340,8 +340,11 @@ class ProductService extends BaseService
     {
         return ProductRecommendation::whereIn('product_id', $products)
             ->with(
+                'product.routes',
+                'product.categories.assets.transforms',
                 'product.variants.tiers',
-                'product.assets.transforms'
+                'product.assets.transforms',
+                'product.firstVariant'
             )
             ->whereHas('product')
             ->select(

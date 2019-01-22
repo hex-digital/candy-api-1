354d09e11ffd003219669be43c3a6593b71fbc35
Alec Ritson <hello@itsalec.co.uk>
2019-01-22 12:03:23 +0000

    Added tests

--- tests/Unit/Baskets/Factories/BasketFactoryTest.php
+++ tests/Unit/Baskets/Factories/BasketFactoryTest.php
@@ -3,14 +3,16 @@
 namespace Tests\Unit\Orders\Services;
 
 use Tests\TestCase;
-use Illuminate\Support\Facades\Event;
 use GetCandy\Api\Core\Baskets\Models\Basket;
-use GetCandy\Api\Core\Baskets\Models\BasketLine;
-use GetCandy\Api\Core\Baskets\Services\BasketService;
-use GetCandy\Api\Core\Products\Models\ProductVariant;
 use GetCandy\Api\Core\Baskets\Factories\BasketFactory;
 use GetCandy\Api\Core\Baskets\Interfaces\BasketFactoryInterface;
 use GetCandy\Api\Core\Products\Factories\ProductVariantFactory;
+use GetCandy\Api\Core\Discounts\Models\DiscountCriteriaItem;
+use GetCandy\Api\Core\Discounts\Models\DiscountReward;
+use GetCandy\Api\Core\Discounts\Models\DiscountCriteriaSet;
+use GetCandy\Api\Core\Discounts\Models\Discount;
+use GetCandy\Api\Core\Baskets\Models\BasketLine;
+use GetCandy\Api\Core\Products\Models\ProductVariant;
 
 /**
  * @group baskets
@@ -66,5 +68,67 @@ class BasketFactoryTest extends TestCase
         $this->assertEquals($basket->total_tax, $taxTotal);
         $this->assertEquals($basket->total_cost, $total);
 
+    }
+
+    /**
+     * @group new
+     */
+    public function test_discount_can_be_added_to_basket()
+    {
+        $variant = ProductVariant::first();
+        $basket = Basket::forceCreate([
+            'currency' => 'GBP',
+        ]);
+
+        BasketLine::forceCreate([
+            'product_variant_id' => $variant->id,
+            'basket_id' => $basket->id,
+            'quantity' => 1,
+            'total' => $variant->price,
+        ]);
+
+        $discount = Discount::forceCreate([
+            'attribute_data' => [
+                'name' => ['en' => 'Test Discount'],
+            ],
+            'status' => 1,
+            'channel_id' => 1,
+            'start_at' => \Carbon\Carbon::now()->startOfDay(),
+            'end_at' => \Carbon\Carbon::now()->endOfDay(),
+            'lower_limit' => 1,
+        ]);
+
+        $criteria = DiscountCriteriaSet::forceCreate([
+            'discount_id' => $discount->id,
+            'scope' => 'all',
+            'outcome' => 1,
+        ]);
+
+        DiscountReward::forceCreate([
+            'discount_id' => $discount->id,
+            'type' => 'percentage',
+            'value' => 10
+        ]);
+
+        DiscountCriteriaItem::forceCreate([
+            'discount_criteria_set_id' => $criteria->id,
+            'type' => 'coupon',
+            'value' => 'TESTCOUPON',
+        ]);
+
+        \DB::table('basket_discount')->insert([
+            'basket_id' => $basket->id,
+            'discount_id' => $discount->id,
+            'coupon' => 'TESTCOUPON',
+        ]);
+
+        $basket = $this->app->make(BasketFactory::class)->init($basket)->get();
+
+        $this->assertCount(1, $basket->discounts);
+
+        $this->assertEquals(1, $basket->discount_total);
+        $this->assertEquals(9, $basket->sub_total);
+        $this->assertEquals(1.80, $basket->total_tax);
+        $this->assertEquals(10.80, $basket->total_cost);
     }
 }
